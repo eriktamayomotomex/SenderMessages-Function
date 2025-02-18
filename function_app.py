766d546540f79e2455a14e5fa2feb981994fbebc
eriktamayomotomex <erik.tamayo@motomex.com>
2025-02-18 17:14:14 -0600

    Fix: Add testing env

--- function_app.py
+++ function_app.py
@@ -1,10 +1,25 @@
 import azure.functions as func
 import logging
-from whatsapp_function import main as whatsapp_main  # Importamos la función de WhatsApp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="enviar_mensajes")
-def enviar_mensajes(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Iniciando el envío de mensajes de WhatsApp.')
-    return whatsapp_main(req)  # Llama a la función principal
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hola, {name}. La función se ejecutó correctamente.")
+    else:
+        return func.HttpResponse(
+            "La función se ejecutó correctamente. Envia un parámetro 'name' para personalizar la respuesta.",
+            status_code=200
+        )
